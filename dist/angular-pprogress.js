f45f48850d501e762104be3d5713a463be4a222e
Greg Allen <me@jga.me>
2013-11-05 08:16:39 -0800

    project setup

---
+++ dist/angular-pprogress.js
@@ -0,0 +1,9 @@
+
+/*!
+ * angular-pprogress - Angular directive for pprogress
+ * v0.1.0
+ * https://github.com/firstandthird/angular-pprogress
+ * copyright First + Third 2013
+ * MIT License
+*/
+//angular-pprogress.js
